438cff5d7800f035bb1554cd5dc761a8ac5178b5
Baptiste <baptiste.chiocca@gmail.com>
2025-10-05 18:04:35 +0200

    Fix prisma client

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
+import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "./generated/prisma";
+
 // If your Prisma file is located elsewhere, you can change the path
 
 const prisma = new PrismaClient();
